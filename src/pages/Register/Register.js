167c016b3efff1045002f71f6309abb9db8a80e1
Muhammad-Salman-Hassan <msalmanhassan288311@gmail.com>
2022-12-21 14:29:28 +0500

    Adding Form validation and submit into database

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,21 +1,59 @@
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Link, Router, useNavigate } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 import fuuastLogo from "../../images/fuusat.png";
 import styles from "./register.module.css";
 import InputMask from "react-input-mask";
-import Alert from '@mui/material/Alert';
+import "animate.css/animate.min.css";
 import { useState } from "react";
+import axios from "axios";
+import { toast, ToastContainer ,cssTransition} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const bounce = cssTransition({
+  enter: "animate__animated animate__bounceIn",
+  exit: "animate__animated animate__bounceOut"
+});
+
 const Register = () => {
-  const cnics = useRef()
-  const [cnic, setcnic] = useState("")
-  const getcnic=(e)=>{
-    setcnic(e.target.value)
-    // console.log(e.current)
-  }
-  console.log(cnic)
+  const navigate = useNavigate();
+
+  const [cnic, setcnic] = useState("");
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    const obj = { name, cnic, email, password };
+    try {
+      const res = await axios.post("http://localhost:3001/register", obj);
+      if (res.status === 200) {
+        toast.dark("Form Submitted SuccesFully !", {
+          position: toast.POSITION.TOP_CENTER,
+          transition: bounce
+
+        });
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      }
+
+      if(res.status===1062){
+        toast.error("User Already Exist !",{
+          position: toast.POSITION.TOP_CENTER,
+          // transition: bounce
+        })
+      }
+    } catch (e) {
+      alert(e);
+      
+    }
+  };
+
   return (
-    <section class="vh-100">
+    <section class="h-100">
+      <ToastContainer />
       <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-lg-12 col-xl-11">
@@ -29,7 +67,7 @@ const Register = () => {
                       Register Now
                     </p>
 
-                    <form class="mx-1 mx-md-4">
+                    <form class="mx-1 mx-md-4" onSubmit={register}>
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
@@ -39,6 +77,10 @@ const Register = () => {
                             class={`${styles.formcontrol}`}
                             placeholder="Your Name"
                             required
+                            onChange={(e) => {
+                              setname(e.target.value);
+                            }}
+                            value={name}
                           />
                         </div>
                       </div>
@@ -52,6 +94,10 @@ const Register = () => {
                             class={`${styles.formcontrol}`}
                             placeholder="Email:abc@gmail.com"
                             required
+                            onChange={(e) => {
+                              setemail(e.target.value);
+                            }}
+                            value={email}
                           />
                         </div>
                       </div>
@@ -60,21 +106,18 @@ const Register = () => {
                         <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
                           <InputMask
-                          ref={cnics}
                             mask="99999-9999999-9"
                             // mask="-------------"
                             type="text"
                             id="form3Example4cd"
                             class={`${styles.formcontrol}`}
                             placeholder="Your Cnic"
-                            onChange={getcnic}
-                            value={cnic} 
+                            onChange={(e) => {
+                              setcnic(e.target.value);
+                            }}
+                            value={cnic}
                             required
                           />
-                            
-                          
-                          
-                          
                         </div>
                       </div>
                       <div class="d-flex flex-row align-items-center mb-4">
@@ -83,14 +126,16 @@ const Register = () => {
                           <input
                             type="password"
                             id="form3Example4c"
+                            value={password}
                             class={`${styles.formcontrol}`}
                             placeholder="Your Password"
                             required
+                            onChange={(e) => {
+                              setpassword(e.target.value);
+                            }}
                           />
                         </div>
                       </div>
-
-                    
 
                       <div class=" d-flex justify-content-center mb-5">
                         {/* <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" /> */}
@@ -99,7 +144,7 @@ const Register = () => {
                             <BsArrowLeft />
                           </i>{" "}
                           <Link to="/login" className={`${styles.color}`}>
-                           Back To Sign In
+                            Back To Sign In
                           </Link>
                         </div>
                       </div>
@@ -109,7 +154,7 @@ const Register = () => {
                           type="submit"
                           class={`btn btn-lg ${styles.registerbtn}`}
                         />
-                          {/* Register
+                        {/* Register
                         </input> */}
                       </div>
                     </form>
