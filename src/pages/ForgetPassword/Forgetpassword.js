4cc7ac485ef9bf7a24e3546cfd9e2ee020cba4c3
Muhammad-Salman-Hassan <msalmanhassan288311@gmail.com>
2023-01-01 13:49:19 +0500

    Changes in Forget PAssword

--- src/pages/ForgetPassword/Forgetpassword.js
+++ src/pages/ForgetPassword/Forgetpassword.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import "./Forgetpassword.css"
+import React, { useState } from "react";
+import "./Forgetpassword.css";
 
 const Forgetpassword = () => {
+  const [email, setemail] = useState("");
+  const submit = (e) => {
+   e.preventDefault()
+   console.log("submit")
+   setemail(' ')
+  };
+  console.log(email);
   return (
     <div class="container d-flex justify-content-center align-items-center vh-100">
-    <div class="bg-white text-center p-5 mt-3 center">
-       <h3 class='heading'>Forgot Password </h3>
-       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-       <form class="pb-3" action="#">
+      <div class="bg-white text-center p-5 mt-3 center">
+        <h3 class="heading">Forgot Password </h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <form class="pb-3" onSubmit={submit}>
           <div class="form-group">
-             <input  type="email" class="form-control user-input" placeholder="Enter your Email" required />
+            <input
+              type="email"
+              class="form-control user-input"
+              placeholder="Enter your Email"
+              required
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
           </div>
-       </form>
-       <button type="button" class="btn reset">Reset Password</button>
+          <button type="submit" class="btn reset">
+          Reset Password
+        </button>
+        </form>
+       
+      </div>
     </div>
- </div>
-  )
-}
+  );
+};
 
-export default Forgetpassword
+export default Forgetpassword;
