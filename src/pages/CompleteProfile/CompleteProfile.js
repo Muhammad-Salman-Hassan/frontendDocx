88b921a6bc31a7e5cccc8b1ed7bacc5c30c339fc
unknown <msalmanhassan288311@gmail.com>
2023-01-08 13:03:49 +0500

    changes in Profile page

--- src/pages/CompleteProfile/CompleteProfile.js
+++ src/pages/CompleteProfile/CompleteProfile.js
@@ -1,119 +1,132 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./completeprofile.css"
+import InputMask from "react-input-mask";
+import {AiOutlineArrowLeft} from "react-icons/ai"
 
 const CompleteProfile = () => {
   return (
-    <div class="container rounded bg-white mt-5">
-      <div class="alert alert-success" role="alert">
+    <div className="continer-fluid h-100 d-flex justify-content-center align-items-center profile_main_wrapper">
+      <div class="container rounded p-5 form_card">
+      <div class="alert alert-warning" role="alert">
  Please Complete Your Profile for Verfication Process !
 </div>
-      <div class="row">
+      <form class="row">
         
         <div class="col-md-4 border-right">
           <div class="d-flex flex-column align-items-center  justify-content-center  p-3 py-5">
             <img
-              class="mt-5 mb-5"
+              class="mt-5 mb-5 rounded-circle"
               src="https://i.imgur.com/0eg0aG0.jpg"
               width="90"
             />
             
             <span class="file-input btn  btn-file">
-                Browse&hellip; <input type="file" multiple/>
+                Browse Image&hellip; <input type="file" multiple/>
             </span>
-            
+            <div class="alert alert-info mt-3" role="alert">
+ Please Upload Your Profile with size 240 X 240 px !
+</div>
           </div>
         </div>
         <div class="col-md-8">
+         
           <div class="p-3 py-5">
             <div class="d-flex justify-content-between align-items-center mb-3">
               <div class="d-flex flex-row align-items-center back">
-                <i class="fa fa-long-arrow-left mr-1 mb-1"></i>
-                <Link to="/userdashboard">Back to home</Link>
+                
+                <Link to="/userdashboard" className="link"> <span><AiOutlineArrowLeft/></span>  Back to home</Link>
               </div>
               
             </div>
             <div class="row mt-2">
+            <h2>Profile</h2>
               <div class="col-md-6">
+              
                 <input
                   type="text"
-                  class="form-control"
-                  placeholder="first name"
-                  value="John"
+                  class="form-control inputform"
+                  placeholder="Full Name"
+                  required
                 />
               </div>
               <div class="col-md-6">
                 <input
                   type="text"
-                  class="form-control"
-                  value="Doe"
-                  placeholder="Doe"
-                />
-              </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col-md-6">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Email"
-                  value="john_doe12@bbb.com"
-                />
-              </div>
-              <div class="col-md-6">
-                <input
-                  type="text"
-                  class="form-control"
-                  value="+19685969668"
-                  placeholder="Phone number"
+                  class="form-control inputform"
+                placeholder="Father Name"
+                required
                 />
               </div>
             </div>
             <div class="row mt-3">
               <div class="col-md-6">
                 <input
-                  type="text"
-                  class="form-control"
-                  placeholder="address"
-                  value="D-113, right avenue block, CA,USA"
+                  type="number"
+                  class="form-control inputform"
+                  placeholder="Roll Number"
+                  required
                 />
               </div>
               <div class="col-md-6">
                 <input
                   type="text"
-                  class="form-control"
-                  value="USA"
-                  placeholder="Country"
+                  class="form-control inputform"
+                  required
+                  placeholder="Department Name"
+                  maxlength="15"
                 />
               </div>
             </div>
             <div class="row mt-3">
               <div class="col-md-6">
                 <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Bank Name"
-                  value="Bank of America"
+                type="number"
+                  min="2000" max="2022" step="1" 
+                  class="form-control inputform"
+                  placeholder="Passing Year Eg.(2015)"
+                  required
                 />
               </div>
               <div class="col-md-6">
                 <input
                   type="text"
-                  class="form-control"
-                  value="043958409584095"
-                  placeholder="Account Number"
+                  class="form-control inputform"
+                  placeholder="Enrollment Number"
+                  required
                 />
               </div>
             </div>
-            <div class="mt-5 text-right">
-              <button class="btn btn-primary profile-button" type="button">
+            <div class="row mt-3">
+              <div class="col-md-6">
+                <InputMask
+                mask="999-999999-99"
+                  type="text"
+                  class="form-control inputform"
+                  placeholder="Phone Number"
+                  required
+                />
+              </div>
+              <div class="col-md-6">
+                <input
+                  type="text"
+                  class="form-control inputform"
+                  placeholder="Library Id"
+                 
+                />
+              </div>
+            </div>
+            <div class="mt-5 text-center">
+              <button class="btn registerbtn profile-button" type="submit">
                 Save Profile
               </button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
+    </div>
+    
   );
 };
 
