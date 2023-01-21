914dd4a3c1cd2b64045d837c3809eb0334118096
unknown <msalmanhassan288311@gmail.com>
2023-01-21 21:11:40 +0500

    Api integration reset password

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -8,11 +8,34 @@ import "./resetPassword.css";
 const ResetPassword = () => {
   let params = useParams();
   const [eyeToggle, seteyeToggle] = useState(false);
-  const [user, setuser] = useState(null)
+  const [user, setuser] = useState()
+  const [state, setState] = React.useState({
+    userpassword: "",
+    confirmPassword: ""
+  })
 
   let toggleEye = () => {
     seteyeToggle(!eyeToggle);
   };
+
+  const handleChange=(e)=>{
+    let value=e.target.value
+
+    setState({...state,[e.target.name]:value})
+  }
+console.log(state)
+
+const handleSubmit=async(e)=>{
+  e.preventDefault()
+  try {
+    let res = await axios.post(
+      `http://localhost:3001/reset-password/${params.id}/${params.token}`,state,{headers: { 'Content-Type': 'application/json' },withCredentials: true}
+    )
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
   let get_user = async () => {
     try {
@@ -20,6 +43,7 @@ const ResetPassword = () => {
         `http://localhost:3001/reset-password/${params.id}/${params.token}`
       );
       setuser(res.data)
+      console.log(res)
     } catch (error) {
       console.log(error);
     }
@@ -28,6 +52,7 @@ const ResetPassword = () => {
   useEffect(() => {
     get_user();
   }, []);
+ 
 
   return (
     <div class="container d-flex justify-content-center align-items-center vh-100 flex-column">
@@ -35,20 +60,18 @@ const ResetPassword = () => {
       <div class="bg-white text-center p-5 mt-1 center">
         <h3 class="heading">Reset Password </h3>
         <p>
-          Hey <span><strong>{user.name}</strong></span> Please Provide Your New Password!
+          Hey <span><strong>{user?.name}</strong></span> Please Provide Your New Password!
         </p>
-        <form class="pb-3">
+        <form class="pb-3" onSubmit={handleSubmit}>
           <div class="form-group d-flex justify-content-center align-items-center px-3 form_wrapper">
             <input
               type={eyeToggle ? "text" : "password"}
               class="form-control user_input"
               placeholder="Enter Your Password"
               required
-
-              // value={email}
-              // onChange={(e) => {
-              //   setemail(e.target.value);
-              // }}
+              name="userpassword"
+              value={state.userpassword}
+              onChange={handleChange}
             />
             <span className="eye" onClick={toggleEye}>
               {eyeToggle?<AiOutlineEyeInvisible/>:<AiFillEye />}
@@ -60,11 +83,9 @@ const ResetPassword = () => {
               class="form-control user_input"
               placeholder="Confirm Your Password"
               required
-
-              // value={email}
-              // onChange={(e) => {
-              //   setemail(e.target.value);
-              // }}
+              name="confirmPassword"
+              value={state.confirmPassword}
+              onChange={handleChange}
             />
             
           </div>
